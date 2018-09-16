b6c077fe08753c5a4b8a1595ccfcd52c527f9de0
Jokajoka <jean.darke@hotmail.com>
2018-09-16 19:46:47 +0300

    Viikko 2 tehtävät 3 ja 4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,19 @@
 class Beer < ApplicationRecord
 	belongs_to :brewery
 	has_many :ratings
+
+	def average_rating
+		ar = 0.0
+		count = 0
+		r = self.ratings
+		r.each do |rating|
+			ar += rating.score
+			count += 1
+		end		   
+		if count != 0
+			return ar / count
+		else
+			return 0.0
+		end
+	end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,9 @@
 class Rating < ApplicationRecord
     belongs_to :beer
+
+    def to_s
+        return "#{self.beer.name} #{self.score}"
+    end
+
+
 end
