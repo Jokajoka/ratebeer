81b9177f8bfd915589c4ff90299c5acff6cbfa26
Jokajoka <jean.darke@hotmail.com>
2018-09-16 23:16:23 +0300

    Viikko 2 tehtävät 11 ja 12

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root 'breweries#index'
-  get 'kaikki_bisset', to: 'beers#index'
-  get 'ratings', to: 'ratings#index'
-  get 'ratings/new', to:'ratings#new'
+  #get 'kaikki_bisset', to: 'beers#index'
+  #get 'ratings', to: 'ratings#index'
+  #get 'ratings/new', to:'ratings#new'
 
-  post 'ratings', to: 'ratings#create'
-  
+  #post 'ratings', to: 'ratings#create'
+
+  resources :ratings, only: [:index, :new, :create, :destroy]
   resources :beers
   resources :breweries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
