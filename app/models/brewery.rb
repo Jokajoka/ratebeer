44aa495a879ed5173eb03d535ff122333c39446f
Jokajoka <jean.darke@hotmail.com>
2018-09-16 23:32:08 +0300

    Viikko 2 tehtävät 13 ja 14

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,6 @@
 class Brewery < ApplicationRecord
-	has_many :beers
+	has_many :beers, dependent: :destroy
+	has_many :ratings, through: :beers
 
 	def print_report
 		puts name
@@ -11,4 +12,21 @@ class Brewery < ApplicationRecord
 		self.year = 2018
 		puts "changed year to #{year}"
 	end
+
+	def average_rating
+		ar = 0.0
+		r = self.ratings
+		count = 0
+		r.each do |rating|
+			ar += rating.score
+			count += 1
+		end	
+
+		if count != 0
+			return ar / count
+		else
+			return 0.0
+		end
+	end
+
 end
